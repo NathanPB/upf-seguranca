271960bc9717f86e01bb720c2042c791ac309b1e
NathanPB <nathan.pbombana@gmail.com>
2020-06-15 10:58:38 -0300

    Server Postgress connection

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,22 @@
-require('dotenv')
+require('dotenv').config();
 const express = require('express')
+const { Sequelize } = require('sequelize');
+
+const { DB_HOST, DB_USERNAME, DB_PWD, DB_NAME } = process.env;
+
+const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PWD, {
+  host: DB_HOST.toString().split(":")[0],
+  port: DB_HOST.toString().split(":")[1],
+  dialect: 'postgres'
+})
 
 const app = express();
 const port = process.env.PORT ?? 8080
 
 
-app.listen(port, () => console.log(`API Server listening on ${port}`))
+app.listen(port, () => {
+  sequelize.authenticate()
+    .then(() => {
+      console.log(`API Server listening on ${port}`)
+    }).catch(console.error)
+})
