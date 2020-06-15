04739b6255c368a628f9a9a45d71c7d2e4f87269
NathanPB <nathan.pbombana@gmail.com>
2020-06-15 10:27:15 -0300

    Init server workspace

---
+++ server/src/index.js
@@ -0,0 +1,8 @@
+require('dotenv')
+const express = require('express')
+
+const app = express();
+const port = process.env.PORT ?? 8080
+
+
+app.listen(port, () => console.log(`API Server listening on ${port}`))
