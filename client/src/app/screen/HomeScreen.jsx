ffc11e888f0c48f1929d7f20635712b9062a442f
NathanPB <nathan.pbombana@gmail.com>
2020-06-21 18:26:29 -0300

    [Client] Create filters for the users datatable

--- client/src/app/screen/HomeScreen.jsx
+++ client/src/app/screen/HomeScreen.jsx
@@ -5,11 +5,16 @@ import { DataTable } from 'primereact/datatable';
 import Styles from './HomeScreen.module.scss';
 import {ProgressSpinner} from 'primereact/progressspinner';
 import {Column} from 'primereact/column';
+import {Calendar} from 'primereact/calendar';
 
 export default function HomeScreen({ api }) {
 
+  const tableRef = React.useRef();
+
   const [me, setMe] = React.useState({});
   const [users, setUsers] = React.useState();
+
+  const [datesFilter, setDatesFilter] = React.useState([]);
 
   // TODO find out why this is not automatically parsing the json string
   React.useEffect(() => {
@@ -29,6 +34,10 @@ export default function HomeScreen({ api }) {
       }).catch(console.error)
   }
 
+  React.useEffect(() => {
+    tableRef.current && tableRef.current.filter(datesFilter, 'createdAt', 'custom')
+  }, [ datesFilter ])
+
 
   const menuItems = [
     {
@@ -42,11 +51,58 @@ export default function HomeScreen({ api }) {
   const { email } = me;
   const isLoading = users === undefined;
 
+  function dateRangeFilter(value) {
+    if (datesFilter[0] && datesFilter[1]) {
+      value = new Date(value);
+      value.setHours(0);
+      value.setMinutes(0);
+
+      const max = new Date(+datesFilter[1] + 86400000)
+      return +value >= +datesFilter[0] && +value <= +max
+    } else {
+      return true;
+    }
+  }
+
+  const rangedDatePicker = <div className="p-inputgroup">
+    <Calendar
+      id="filterCalendar"
+      placeholder="Filter by Date"
+      maxDate={new Date()}
+      value={datesFilter}
+      onChange={({ value }) => setDatesFilter(value)}
+      selectionMode="range"
+      style={{ width: '100%' }}
+      inputStyle={{ width: '100%' }}
+      readOnlyInput
+    />
+    <span
+      className="p-inputgroup-addon"
+      title="Clear"
+      onClick={() => setDatesFilter([])}
+      style={{ cursor: 'pointer' }}
+    >Clear</span>
+  </div>
+
   function renderTable() {
     return (
-      <DataTable value={users}>
-        <Column field="email" header="Email" sortable/>
-        <Column field="createdAt" header="Creation Date" sortable/>
+      <DataTable value={users} ref={tableRef} emptyMessage="No Users Found">
+        <Column
+          field="email"
+          header="Email"
+          filterPlaceholder="Search by Email"
+          filterMatchMode="contains"
+          sortable
+          filter
+        />
+        <Column
+          field="createdAt"
+          header="Creation Date"
+          filterElement={rangedDatePicker}
+          filterFunction={dateRangeFilter}
+          sortable
+          filter
+        />
       </DataTable>
     )
   }
